b1128eea218760a2a7d1789c7f312c5784e6a76f
McFriesMcBurger <sengco.ralph@gmail.com>
2023-09-28 16:34:38 +0400

    Final

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -53,12 +53,11 @@ class Book {
     }
 
     // Note: Additional method I added to print the details of each book
-    public void printDetails(int index) {
-        System.out.println("\nBook #" + (index + 1) + ":");
-        System.out.print("Book ID - " + getBookId());
-        System.out.print(" | Title - " + getTitle());
-        System.out.print(" | Author - " + getAuthor());
-        System.out.println(" | Number of Pages - " + getNumberofPages());
+    public void printDetails() {
+        System.out.println("\nBook ID: " + bookId);
+        System.out.print("Title - " + title);
+        System.out.print(" | Author - " + author);
+        System.out.println(" | Number of Pages - " + numberofPages);
     }
 }
 
@@ -75,35 +74,37 @@ class Library {
 
     Scanner scanner = new Scanner(System.in);
 
-    // Method to add a book to the library
-    public void addBook() {
+    // Note: Additional method I added to make books and subsequently add them to
+    // the ArrayList Library
+    public void makeBook() {
         // Initialize variables and prompt user to enter the amount of books to add
         try {
             System.out.print("\nEnter the number of books to add: ");
-
             int bookstoAdd = scanner.nextInt();
-            int bookId;
-            String title;
-            String author;
-            int numberofPages;
 
-            // Iterates through the user's given integer to add that many books to the
-            // Library ArrayList, with each book's details
+            // Iterates through the user's given integer to make that many books to the
+            // with each book's details
             for (int i = 1; i <= bookstoAdd; i++) {
                 try {
                     System.out.println("\nEnter details for Book #" + i + ":");
 
                     System.out.print("Book ID: ");
-                    bookId = scanner.nextInt();
+                    int bookId = scanner.nextInt();
 
                     System.out.print("Title: ");
-                    title = scanner.next();
+                    String title = scanner.next();
 
                     System.out.print("Author: ");
-                    author = scanner.next();
+                    String author = scanner.next();
 
                     System.out.print("Number of Pages: ");
-                    numberofPages = scanner.nextInt();
+                    int numberofPages = scanner.nextInt();
+
+                    // Create a Book object with user-provided details and call the addBook method
+                    // to add each new Book object to the ArrayList
+                    Book book = new Book(bookId, title, author, numberofPages);
+                    addBook(book);
+
                     // This exception block has the added feature of repeating the iteration
                 } catch (InputMismatchException e) {
                     System.out.println("\nInvalid entry. Please try again.");
@@ -111,10 +112,6 @@ class Library {
                     i--;
                     continue;
                 }
-
-                // Create a Book object with user-provided details and add it to the ArrayList
-                Book book = new Book(bookId, title, author, numberofPages);
-                library.add(book);
             }
             // Catches an exception if the user inputs any variable type other than an
             // integer
@@ -125,76 +122,63 @@ class Library {
         }
     }
 
+    // Method to add a book to the library
+    public void addBook(Book book) {
+        library.add(book);
+    }
+
     // Method to display all books in the library
     public void displayBooks() {
         // Simply loops through the Library ArrayList and calls the printDetails method
         // per Book object
         if (!checkEmpty()) {
-            for (int i = 0; i < library.size(); i++) {
-                (library.get(i)).printDetails(i);
+            for (Book book : library) {
+                book.printDetails();
             }
         }
     }
 
     // Method to search for a book by title
-    public void searchBook() {
+    public Book searchBook(String title) {
         // Loops through the Library ArrayList and finds the first Book object with an
-        // identical title, then prints its details and breaks the loop
+        // identical title, then returns the Book object and breaks the loop
         // LIMITATIONS: The title input by the user and the title of the book must be an
         // EXACT match; additionally, if there is more than one book with an identical
         // title, the first in order will be chosen
-        if (!checkEmpty()) {
-            System.out.print("\nEnter the title of the book: ");
-            String title = scanner.next();
-
-            boolean found = false;
-            for (int i = 0; i < library.size(); i++) {
-                if (title.equals((library.get(i)).getTitle())) {
-                    System.out.println("\nMatch found!");
-                    (library.get(i)).printDetails(i);
-                    found = true;
-                    break;
-                }
-            }
-
-            // If there are no Book objects found with an identical title, the user is told
-            // that a match is not found
-            if (!found) {
-                System.out.println("\nMatch not found.");
+        for (int i = 0; i < library.size(); i++) {
+            if (title.equals((library.get(i)).getTitle())) {
+                System.out.println("\nMatch found!");
+                return library.get(i);
             }
         }
-
+        return null;
     }
 
     // Method to remove a book from the library by bookId
-    public void removeBook() {
+    public void removeBook(int bookId) {
         // Loops through the Library ArrayList and finds the first Book object with an
         // identical ID, then removes it from the ArrayList and breaks the loop
         // LIMITATIONS: If there is more than one book with the same ID, only the book
         // that is first in order will be removed, though logically there should not be
         // more than one book with the same ID in practice anyway
-        if (!checkEmpty()) {
-            try {
-                System.out.print("\nEnter the ID of the book: ");
-                int bookId = scanner.nextInt();
-                boolean found = false;
-                for (int i = 0; i < library.size(); i++) {
-                    if (bookId == (library.get(i)).getBookId()) {
-                        System.out.println("\nBook " + (i + 1) + " with ID " + bookId + " has been removed.");
-                        library.remove(i);
-                        found = true;
-                        break;
-                    }
+        try {
+            boolean found = false;
+            for (int i = 0; i < library.size(); i++) {
+                if (bookId == (library.get(i)).getBookId()) {
+                    System.out.println("\nBook " + (i + 1) + " with ID " + bookId + " has been removed.");
+                    library.remove(i);
+                    found = true;
+                    break;
                 }
-                // If no Book object has a matching ID, the user is told that the book ID is not
-                // found
-                if (!found) {
-                    System.out.println("\nBook ID not found.");
-                }
-            } catch (Exception e) {
-                System.out.println("\nInvalid entry. Please try again.");
-                scanner.nextLine();
             }
+            // If no Book object has a matching ID, the user is told that the book ID is not
+            // found
+            if (!found) {
+                System.out.println("\nBook ID not found.");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("\nInvalid entry. Please try again.");
+            scanner.nextLine();
         }
     }
 
@@ -227,36 +211,55 @@ public class LibraryApp {
         // exceptions via incorrect inputs are handled
         try {
             while (true) {
-                System.out.println(
-                        "\n1 - Add Book | 2 - Display Books | 3 - Search Book | 4 - Remove Book | 0 - Terminate Program");
+                try {
+                    System.out.println(
+                            "\n1 - Add Book | 2 - Display Books | 3 - Search Book | 4 - Remove Book | 0 - Terminate Program");
 
-                // Check if the next input is an integer
-                if (scanner.hasNextInt()) {
                     selection = scanner.nextInt();
 
                     switch (selection) {
                         case 1:
-                            library.addBook();
+                            library.makeBook();
                             break;
+
                         case 2:
                             library.displayBooks();
                             break;
+
                         case 3:
-                            library.searchBook();
+                            if (!library.checkEmpty()) {
+                                System.out.print("Enter the title of the book: ");
+                                Book searchedBook = library.searchBook(scanner.next());
+                                // Assuming a Book object is returned, prints its details
+                                if (searchedBook != null) {
+                                    searchedBook.printDetails();
+                                }
+                                // If there are no Book objects found with an identical title, the user is told
+                                // that a match is not found
+                                else {
+                                    System.out.println("\nMatch not found.");
+                                }
+                            }
                             break;
+
                         case 4:
-                            library.removeBook();
+                            if (!library.checkEmpty()) {
+                                System.out.print("Enter the ID of the book: ");
+                                library.removeBook(scanner.nextInt());
+                            }
                             break;
+
                         case 0:
                             return; // Terminates the program
+
                         default:
                             System.out.println("\nInvalid entry. Please try again.");
                             scanner.nextLine(); // Consumes the invalid input
                             break;
                     }
-                } else {
+                } catch (InputMismatchException e) {
                     // Handles a non-integer input
-                    System.out.println("\nInvalid entry. Please enter a valid integer.");
+                    System.out.println("\nInvalid entry. Please try again.");
                     scanner.next(); // Consume the invalid input
                 }
             }
